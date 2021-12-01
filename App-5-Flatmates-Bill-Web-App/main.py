27253f010824be537f3b744381398a9b3564c552
Shubh Agarwal <sazorinos1111@gmail.com>
2021-12-01 23:40:06 +0530

    adding modules made by me

---
+++ App-5-Flatmates-Bill-Web-App/main.py
@@ -0,0 +1,14 @@
+from flask.views import MethodView
+from wtforms import Form
+
+class HomePage():
+    pass
+
+class BillFormPage():
+    pass
+
+class ResultsPage():
+    pass
+
+class BillForm():
+    pass
