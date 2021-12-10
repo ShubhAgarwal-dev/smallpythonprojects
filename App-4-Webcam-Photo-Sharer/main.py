5d9700a0c5c400c6caa200d5c72d0eac8a45dff9
Shubh Agarwal <sazorinos1111@gmail.com>
2021-12-10 14:06:07 +0530

    trying to get it up  working

--- App-4-Webcam-Photo-Sharer/main.py
+++ App-4-Webcam-Photo-Sharer/main.py
@@ -1,16 +1,47 @@
+# To Develop frontend
 import kivy
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+
+# to get image
+from wikipedia import page
+import requests
+
+# to get extension of the image received
+from os.path import splitext
 
 kivy.require('2.0.0')
 Builder.load_file('frontend.kv')
 
 
 class FirstScreen(Screen):
-    # add class name within <class_name> in frontend.kv file
+    #   # add Screen class name within <class_name> in frontend.kv file
+    # -> root in frontend.kv is equal to FirstScreen()
+
+    @staticmethod
+    def get_image(query):
+        image_link = page(query).images[0]
+        return image_link
+
+    @staticmethod
+    def download_image(query):
+        image_link = FirstScreen.get_image(query)
+        response = requests.get(image_link)
+        ext = splitext(image_link)[-1].lower()
+        with open(fr'files/{query}{ext}', mode='wb') as file:
+            file.write(response.content)
+        return fr'files/{query}{ext}'
+
     def search_image(self):
-        pass
+        query = self.manager.current_screen.ids.user_query.text
+        # Get user query from text input
+        print('working...')
+        self.manager.current_screen.ids.img.source = self.get_image(query)
+        # id: img defined in frontend.kv
+        # Display the output
+        print(self.get_image(query))
+        print(type(self.get_image(query)))
 
 
 class RootWidget(ScreenManager):
