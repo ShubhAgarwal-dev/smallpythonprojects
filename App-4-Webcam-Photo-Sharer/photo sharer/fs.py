bdb675ed7f8270edb0941bc987c14ff30d12d757
Shubh Agarwal <sazorinos1111@gmail.com>
2021-12-11 12:00:14 +0530

    can not know why is it a missing module

---
+++ App-4-Webcam-Photo-Sharer/photo sharer/fs.py
@@ -0,0 +1,6 @@
+class FileSharer():
+    pass
+
+
+if __name__ == '__main__':
+    print("Hello World")
--- App-4-Webcam-Photo-Sharer/photo sharer/main.py
+++ App-4-Webcam-Photo-Sharer/photo sharer/main.py
@@ -4,8 +4,28 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
+# to share file across
+from fs import FileSharer
+
 kivy.require('2.0.0')
 Builder.load_file('frontend.kv')
+
+
+class CameraScreen(Screen):
+    # Using it as Webcam Class
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def capture(self):
+        pass
+
+
+class ImageScreen(Screen):
+    pass
 
 
 # For Description for each of this look into photo searcher for the code
@@ -19,4 +39,4 @@ class MainApp(App):
         return RootWidget()
 
 
-MainApp()
+MainApp().run()
