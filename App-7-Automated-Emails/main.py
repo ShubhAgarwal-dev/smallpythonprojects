07ed87c36dfcb6f765b380bfac8fdcf77135493a
Shubh Agarwal <sazorinos1111@gmail.com>
2021-12-19 18:01:16 +0530

    now made

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -1,76 +1,26 @@
-from newsapi import NewsApiClient as NAC
-import apikey  # sorry cannot share that
-from datetime import date
+import yagmail
+import pandas as pd
+from news import NewsFeed  # for news feed object
 
+import apikey  # it contains my email address and password
 
-class NewsFeed():
-    '''
-    Repersenting multiple news titles and urls 
-    as part of a single string.
-    '''
+import datetime
 
-    apikey = apikey.key
-    newsapi = NAC(api_key=apikey)
+today = datetime.datetime.now().strftime('%Y-%m-%d')
+yesterday = (datetime.datetime.now() -
+             datetime.timedelta(days=2)).strftime('%Y-%m-%d')
 
-    def __init__(self, intrest, from_date, to_date, language) -> None:
-        self._intrest = intrest
-        self._from_date = from_date
-        self._to_date = to_date
-        self._lang = language
+df = pd.read_excel('people.xlsx')
 
-    def get(self):
-        '''
-        To get the news article for specific intrest of people
-        '''
-        all_articles = self.newsapi.get_everything(
-            qintitle=self._intrest,
-            from_param=self._from_date,
-            to=self._to_date,
-            language=self._lang,
-            sort_by='relevancy')
+for i, row in df.iterrows():
+    interest = row['interest']
+    news_feed = NewsFeed(intrest=interest, from_date=today, to_date=yesterday)
 
-        articles = all_articles['articles']
-        single_string = ''
-
-        for article in articles:
-            article_title = articles['title']
-            article_url = article['url']
-            single_string += article_title + '\n' + article_url + '\n\n'
-
-        return single_string
-
-    def get10(self):
-        '''
-        Not to get full bucket load of news stuff
-        '''
-        all_articles = self.newsapi.get_everything(
-            qintitle=self._intrest,
-            from_param=self._from_date,
-            to=self._to_date,
-            language=self._lang,
-            sort_by='relevancy')
-
-        articles = all_articles['articles']
-        single_string = ''
-
-        for i, article in enumerate(articles):
-            if i >= 10:
-                break
-            article_title = article['title']
-            article_url = article['url']
-            single_string += article_title + '\n' + article_url + '\n\n'
-
-        return single_string
-
-
-if __name__ == '__main__':
-    # Only to test out code
-    today = date.today()
-    news_feed = NewsFeed(
-        intrest='bitcoin',
-        from_date=f'{today.year}-{today.month}-{int(today.day) - 1}',
-        to_date=str(today),
-        language='en')
-
-with open('test.txt', mode='w') as file:
-    file.write(news_feed.get10())
+    email = yagmail.SMTP(user=apikey.email_id, password=apikey.email_password)
+    receiver_email = row['email']
+    receiver_name = row['name']
+    email.send(
+        to=receiver_email,
+        subject=f'Hello {receiver_name}, here is your news for today',
+        contents=f"Hi {receiver_name}, see what's on about {interest} for today,\n{news_feed.get10()}"
+    )
