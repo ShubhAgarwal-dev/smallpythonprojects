004aedc9ba49218001bd3ffd08510c31c9f37869
Shubh Agarwal <sazorinos1111@gmail.com>
2022-01-07 18:46:30 +0530

    Just added the ciname class

--- App-10-Cinema-Ticket-Booking/cinema/db.py
+++ App-10-Cinema-Ticket-Booking/cinema/db.py
@@ -1,10 +1,22 @@
 import sqlite3
+from os.path import isfile
+from string import ascii_uppercase
 
 
 class Banking():
+    """
+    Just a databasse class, performs all the transactions
+    required for booking the ticket
+    """
 
     def __init__(self, database_path: str = 'banking.db') -> None:
+        """
+        Create the database if it does not exist by default.
+        """
         self._database_path = database_path
+        if not isfile(path=database_path):
+            self._make_database(self._database_path)
+            # now it would automatically make the database if not already formed
 
     def add_user(self, card_type: str, card_number: int, cvc: int, holder: str,
                  balance: float):
@@ -35,7 +47,11 @@ class Banking():
         connection.close()
         for i, value in enumerate(result):
             balance = result[i][0]
-        return balance
+        while True:
+            try:
+                return balance
+            except UnboundLocalError:
+                return "Check your card number and cvc anf try again."
 
     def deduct_amount(self, card_number: int, cvc: int, amount: float):
         initial_amount = self.read_balance(card_number, cvc)
@@ -78,7 +94,49 @@ class Banking():
         """)
         connection.commit()
         connection.close()
+        return None
 
 
 class Cinema():
-    pass
+
+    def __init__(self, database_path: str = 'cinema.db') -> None:
+        self._database_path = database_path
+        if not isfile(database_path):
+            self._make_database(self._database_path)
+            price = 100
+            for i, letter in enumerate(ascii_uppercase):
+                for m in range(1, 11):
+                    seat_id = letter + str(m)
+                    self.add_seats(seat_id, 0, price)
+                if i > 8:
+                    break
+                price += 10
+
+    @staticmethod
+    def _make_database(db_path: str) -> None:
+        connection = sqlite3.connect(db_path)
+        connection.execute("""
+            CREATE TABLE "Seat" (
+                "seat_id"	TEXT,
+                "taken"	INTEGER,
+                "price"	REAL
+            );
+
+        """)
+        connection.commit()
+        connection.close()
+        return None
+
+    def add_seats(self, seat_id: str, taken: bool, price: float) -> None:
+        connection = sqlite3.connect(self._database_path)
+        connection.execute(
+            """
+            INSERT INTO "Seat"("seat_id", "taken", "price") VALUES(?,?,?)
+        """, [seat_id, int(taken), float(price)])
+        connection.commit()
+        connection.close()
+        return None
+
+
+if __name__ == "__main__":
+    theare_seat_db = Cinema()
---
+++ App-10-Cinema-Ticket-Booking/cinema/etc/readme.txt
@@ -0,0 +1,2 @@
+Got these database made by default, I am adding them to etc folder if required to refer to them in case required.
+Otherwise we would only be working with the once made by me.
